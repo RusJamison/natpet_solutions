574541ef947c4626f59c158d0dcff287b0dfe6e1
Rusakaniko Jamison <jamisonr11@gmail.com>
2025-01-09 22:44:03 +0000

    import Product in basket model.py

--- basket/models.py
+++ basket/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-#from products.models import Product
+from products.models import Product
 from users.models import Profile
 # Create your models here.
 
@@ -33,7 +33,7 @@ class BasketItem(models.Model):
     An item in a basket. Relates to a product and includes quantity.
     """
     basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='basket_items')
-    #product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
     created_at = models.DateTimeField(auto_now_add=True)
 
