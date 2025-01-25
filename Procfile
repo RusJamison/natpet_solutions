aa895b82f25a929884ec1586e477fbc3428f247a
Rusakaniko Jamison <jamisonr11@gmail.com>
2025-01-25 09:53:59 +0000

    Fix allowed hosts

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn boutique_ado.wsgi:application
+web: gunicorn natpet_solutions.wsgi:application
--- natpet_solutions/settings.py
+++ natpet_solutions/settings.py
@@ -29,10 +29,13 @@ SECRET_KEY = 'django-insecure-q#lnieydpme@t#ixj)!%*=m_)3_+ge&_tc$ji6pt^$zw*&v4_v
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rusjamison-natpetsoluti-muxblq7cukq.ws.codeinstitute-ide.net']
-CSRF_TRUSTED_ORIGINS = ['https://8000-rusjamison-natpetsoluti-muxblq7cukq.ws.codeinstitute-ide.net'
+ALLOWED_HOSTS = ['8080-rusjamison-natpetsoluti-muxblq7cukq.ws.codeinstitute-ide.net',
+'natpet-solutions-559b26bc33e6.herokuapp.com'
+]
+CSRF_TRUSTED_ORIGINS = ['https://8000-rusjamison-natpetsoluti-muxblq7cukq.ws.codeinstitute-ide.net',
+'https://natpet-solutions-559b26bc33e6.herokuapp.com'
 ]
 
 
