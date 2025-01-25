5c77ffb85163cd4afd963fc75b04d649b85b5597
Rusakaniko Jamison <jamisonr11@gmail.com>
2025-01-25 10:41:49 +0000

    Add whitenoise

--- natpet_solutions/settings.py
+++ natpet_solutions/settings.py
@@ -102,6 +102,7 @@ LOGIN_REDIRECT_URL = '/'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
