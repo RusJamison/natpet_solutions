6f0669562bfd2314f398ec3672b390c8251f7912
RusJamison <jamisonr11@gmail.com>
2025-03-02 11:04:12 +0000

    Commit with debug false

--- natpet_solutions/settings.py
+++ natpet_solutions/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
 
 ALLOWED_HOSTS = [
 '8000-rusjamison-natpetsoluti-muxblq7cukq.ws.codeinstitute-ide.net',
